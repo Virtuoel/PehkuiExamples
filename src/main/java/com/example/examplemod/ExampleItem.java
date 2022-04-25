ae8a5c58ae137f993687cc01abb566b58f551f1d
Virtuoel <Virtuoel@users.noreply.github.com>
2022-04-25 22:50:47 +0300

    1.17.1

--- src/main/java/com/example/examplemod/ExampleItem.java
+++ src/main/java/com/example/examplemod/ExampleItem.java
@@ -1,11 +1,11 @@
 package com.example.examplemod;
 
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.world.World;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import virtuoel.pehkui.api.ScaleTypes;
 
 public class ExampleItem extends Item {
@@ -14,11 +14,11 @@ public class ExampleItem extends Item {
 	}
 
 	@Override
-	public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+	public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
 		if (!world.isClientSide) {
 			ScaleTypes.BASE.getScaleData(player).setTargetScale(player.isShiftKeyDown() ? 1.0F : 2.0F);
 		}
 
-		return ActionResult.success(player.getItemInHand(hand));
+		return InteractionResultHolder.success(player.getItemInHand(hand));
 	}
 }
